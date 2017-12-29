02370d6cac07897f5ac3ec34e31e4d98aa230dec
MaximSemenov <maxim.n.semenov@gmail.com>
2017-12-28 22:23:37 -0800

    Coordinates  is added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Chessboard from './modules/board';
+import Pieces from './modules/pieces';
 
 const chessBoard = new Chessboard('chessBoard');
-
 chessBoard.setChessBoard(0, 8);
+console.log (chessBoard);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/app',
 
     output: {
-        path: path.resolve (__dirname, 'docs'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
 
